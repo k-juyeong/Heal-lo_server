3d9c15ecf67f8357fd4cc54408cae5f9173b9ae7
ttkkddms <tkddms603@gmail.com>
2022-09-28 18:07:25 +0900

    게시판 수정중

--- src/main/java/com/kh/heallo/domain/board/dao/BoardDAOImpl.java
+++ src/main/java/com/kh/heallo/domain/board/dao/BoardDAOImpl.java
@@ -54,9 +54,9 @@ public class BoardDAOImpl implements BoardDAO{
   @Override
   public List<Board> findAll() {
     StringBuffer sql = new StringBuffer();
-    sql.append("  select no, t3.bdno, t3.bdcg, t3.bdtitle, t3.memno, t3.bdcontent, t3.bdcdate, t3.memnickname ");
+    sql.append("  select no, t3.bdno, t3.bdcg, t3.bdtitle, t3.memno, t3.bdcontent, t3.bdcdate,t3.bdview, t3.memnickname ");
     sql.append(" from ( ");
-    sql.append("     select rownum no, t1.bdno, t1.bdcg, t1.bdtitle, t1.memno, t1.bdcontent, t1.bdcdate, t2.memnickname ");
+    sql.append("     select rownum no, t1.bdno, t1.bdcg, t1.bdtitle, t1.memno, t1.bdcontent, t1.bdcdate, t1.bdview, t2.memnickname ");
     sql.append("       from board t1, member t2 ");
     sql.append("      where t1.memno=t2.memno ");
     sql.append("      order by t1.bdno desc)t3 ");
@@ -70,9 +70,9 @@ public class BoardDAOImpl implements BoardDAO{
   @Override
   public List<Board> findAll(int startRec, int endRec) {
     StringBuffer sql = new StringBuffer();
-    sql.append("  select no, t3.bdno, t3.bdcg, t3.bdtitle, t3.memno, t3.bdcontent, t3.bdcdate, t3.memnickname ");
+    sql.append("  select no, t3.bdno, t3.bdcg, t3.bdtitle, t3.memno, t3.bdcontent, t3.bdcdate, t3.bdview, t3.memnickname ");
     sql.append(" from ( ");
-    sql.append("     select rownum no, t1.bdno, t1.bdcg, t1.bdtitle, t1.memno, t1.bdcontent, t1.bdcdate, t2.memnickname ");
+    sql.append("     select rownum no, t1.bdno, t1.bdcg, t1.bdtitle, t1.memno, t1.bdcontent, t1.bdcdate, t1.bdview, t2.memnickname ");
     sql.append("       from board t1, member t2 ");
     sql.append("      where t1.memno=t2.memno ");
     sql.append("      order by t1.bdno desc)t3 ");
@@ -87,9 +87,9 @@ public class BoardDAOImpl implements BoardDAO{
   @Override
   public List<Board> findAll(String bdcg, int startRec, int endRec) {
     StringBuffer sql = new StringBuffer();
-    sql.append("  select no, t3.bdno, t3.bdcg, t3.bdtitle, t3.memno, t3.bdcontent, t3.bdcdate, t3.memnickname ");
+    sql.append("  select no, t3.bdno, t3.bdcg, t3.bdtitle, t3.memno, t3.bdcontent, t3.bdcdate, t3.bdview, t3.memnickname ");
     sql.append(" from ( ");
-    sql.append("     select rownum no, t1.bdno, t1.bdcg, t1.bdtitle, t1.memno, t1.bdcontent, t1.bdcdate, t2.memnickname ");
+    sql.append("     select rownum no, t1.bdno, t1.bdcg, t1.bdtitle, t1.memno, t1.bdcontent, t1.bdcdate, t1.bdview, t2.memnickname ");
     sql.append("       from board t1, member t2 ");
     sql.append("      where t1.memno=t2.memno ");
     sql.append("      order by t1.bdno desc)t3 ");
@@ -105,9 +105,9 @@ public class BoardDAOImpl implements BoardDAO{
   @Override
   public List<Board> findAll(BbsFilterCondition filterCondition) {
     StringBuffer sql = new StringBuffer();
-    sql.append("  select no, t3.bdno, t3.bdcg, t3.bdtitle, t3.memno, t3.bdcontent, t3.bdcdate, t3.memnickname ");
+    sql.append("  select no, t3.bdno, t3.bdcg, t3.bdtitle, t3.memno, t3.bdcontent, t3.bdcdate, t3.bdview, t3.memnickname ");
     sql.append(" from ( ");
-    sql.append("     select rownum no, t1.bdno, t1.bdcg, t1.bdtitle, t1.memno, t1.bdcontent, t1.bdcdate, t2.memnickname ");
+    sql.append("     select rownum no, t1.bdno, t1.bdcg, t1.bdtitle, t1.memno, t1.bdcontent, t1.bdcdate, t1.bdview, t2.memnickname ");
     sql.append("       from board t1, member t2 ");
     sql.append("      where t1.memno=t2.memno ");
     sql.append("      order by t1.bdno desc)t3 ");
@@ -137,11 +137,13 @@ public class BoardDAOImpl implements BoardDAO{
     }
 
     sql.append(" order by no asc ");
+
     List<Board> boards = jt.query(sql.toString(),
         new BeanPropertyRowMapper<>(Board.class),
         filterCondition.getCategory(),
         filterCondition.getStartRec(),
         filterCondition.getEndRec());
+
     return boards;
   }
 
@@ -151,7 +153,7 @@ public class BoardDAOImpl implements BoardDAO{
   @Override
   public Optional<Board> findByBoardId(Long boardId) {
     StringBuffer sql = new StringBuffer();
-    sql.append("select B.bdno, B.bdcg, B.bdtitle, B.bdcdate, B.bdudate, B.bdcontent,B.memno, M.memnickname ");
+    sql.append("select B.bdno, B.bdcg, B.bdtitle, B.bdcdate, B.bdudate, B.bdcontent,B.memno, B.bdview, M.memnickname ");
     sql.append("  from board B, member M ");
     sql.append(" where M.memno=B.memno and B.bdno= ? ");
 
@@ -214,18 +216,19 @@ public class BoardDAOImpl implements BoardDAO{
     return affectedRow;
   }
 
+
+
   @Override
   public int totalCount(BbsFilterCondition filterCondition) {
     StringBuffer sql = new StringBuffer();
-    sql.append(" select count(*)");
-    sql.append("  from ( select row_number() over(order by bdcdate ASC) no, ");
-    sql.append("                bdno, bdcg, bdtitle, memno, bdcontent, bdcdate ");
-    sql.append("          from board  ");
-    sql.append("      order by no desc)B, member M ");
-    sql.append("  where M.memno = B.memno ");
-    sql.append("  and B.no between ? and ? ");
-    sql.append("  and B.bdcg = ? ");
-
+    sql.append("  select no, t3.bdno, t3.bdcg, t3.bdtitle, t3.memno, t3.bdcontent, t3.bdcdate, t3.bdview, t3.memnickname ");
+    sql.append(" from ( ");
+    sql.append("     select rownum no, t1.bdno, t1.bdcg, t1.bdtitle, t1.memno, t1.bdcontent, t1.bdcdate, t1.bdview, t2.memnickname ");
+    sql.append("       from board t1, member t2 ");
+    sql.append("      where t1.memno=t2.memno ");
+    sql.append("      order by t1.bdno desc)t3 ");
+    sql.append(" where no between ? and ? ");
+    sql.append("      and t3.bdcg = ? ");
 
     if(!StringUtils.isEmpty(filterCondition.getCategory()) ||
         !StringUtils.isEmpty(filterCondition.getSearchType()) ||
@@ -235,16 +238,16 @@ public class BoardDAOImpl implements BoardDAO{
 
     switch (filterCondition.getSearchType()){
       case "A": //전체
-        sql.append("   (B.bdtitle like '%"+filterCondition.getKeyword()+ "%' or ");
-        sql.append("    B.bdcontent like '%"+filterCondition.getKeyword() + "%' or ");
-        sql.append("    M.memnickname like '%"+filterCondition.getKeyword()+ "%' ) ");
+        sql.append("   (t3.bdtitle like '%"+filterCondition.getKeyword()+ "%' or ");
+        sql.append("    t3.bdcontent like '%"+filterCondition.getKeyword() + "%' or ");
+        sql.append("    t3.memnickname like '%"+filterCondition.getKeyword()+ "%' ) ");
         break;
       case "N": //닉네임
-        sql.append("    M.memnickname like '%"+filterCondition.getKeyword() +"%'  ");
+        sql.append("    t3.memnickname like '%"+filterCondition.getKeyword() +"%'  ");
         break;
       case "TC": //제목, 내용
-        sql.append("   (B.bdtitle like '%"+filterCondition.getKeyword() +"%' or ");
-        sql.append("    B.bdcontent like '%"+ filterCondition.getKeyword() + "%' ) ");
+        sql.append("   (t3.bdtitle like '%"+filterCondition.getKeyword() +"%' or ");
+        sql.append("    t3.bdcontent like '%"+ filterCondition.getKeyword() + "%' ) ");
         break;
       default:
     }
@@ -255,6 +258,20 @@ public class BoardDAOImpl implements BoardDAO{
         filterCondition.getCategory());
 
     return affectedRow;
+
+  }
+
+
+
+  @Override
+  public int increaseViewCount(Long boardId) {
+    StringBuffer sql = new StringBuffer();
+    sql.append("update board  ");
+    sql.append("set bdview = bdview + 1 ");
+    sql.append("where bdno = ? ");
+
+    int affectedRows = jt.update(sql.toString(), boardId);
+    return affectedRows;
   }
 
 
