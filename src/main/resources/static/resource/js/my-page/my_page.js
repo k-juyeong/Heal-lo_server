5f6bfdfe3283215a2bda4d8f068cff7a733a47f2
bakzh <park3096118@gmail.com>
2022-10-04 13:37:35 +0900

    마이페이지 정보수정로직 수정중

--- src/main/resources/static/resource/js/my-page/my_page.js
+++ src/main/resources/static/resource/js/my-page/my_page.js
@@ -6,6 +6,8 @@
     location.href = url;
   });
 
+    const $ nnameCk = document.querySelector('.nnameCk');
+
   //중복확인 버튼 클릭
   btnCheck.addEventListener('click',e=>{
     const url = `/members/nickname-check?nickname=${nname.value}`
@@ -21,14 +23,14 @@
             if (data.header.code == '00') {
                 console.log(data);
                 document.getElementById('nname').style.border = '3px solid blue';
-                document.querySelector('.nnameCk').innerHTML = '사용가능한 닉네임입니다';
-                document.querySelector('.nnameCk').classList.remove('nnameCkRed');
-                document.querySelector('.nnameCk').classList.add('nnameCkBlue');
+                $nnameCk.innerHTML = '사용가능한 닉네임입니다';
+                $nnameCk.classList.remove('nnameCkRed');
+                $nnameCk.classList.add('nnameCkBlue');
             } else if (data.header.code == '01') {
                 document.getElementById('nname').style.border = '3px solid red';
-                document.querySelector('.nnameCk').innerHTML = '동일한 닉네임이 존재합니다';
-                document.querySelector('.nnameCk').classList.remove('nnameCkBlue');
-                document.querySelector('.nnameCk').classList.add('nnameCkRed');
+                $nnameCk.innerHTML = '동일한 닉네임이 존재합니다';
+                $nnameCk.classList.remove('nnameCkBlue');
+                $nnameCk.classList.add('nnameCkRed');
             } else {
                 throw new Error(data.data);
             }
