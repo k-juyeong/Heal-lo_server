20b9c81d80a50b21f85d9c2e143efd2337a1e91e
bakzh <park3096118@gmail.com>
2022-10-04 14:27:23 +0900

    중복체크 로직 추가

--- src/main/resources/static/resource/js/my-page/my_page.js
+++ src/main/resources/static/resource/js/my-page/my_page.js
@@ -6,8 +6,20 @@
     location.href = url;
   });
 
-    const $ nnameCk = document.querySelector('.nnameCk');
+    const $nnameCk = document.querySelector('.nnameCk');
+    const $btnChange = document.getElementById('btnChange');
+    const $form = document.getElementById('form');
+    let dupNick = false;
 
+    $btnChange.addEventListener('click',e=>{
+        e.preventDefault();
+        if(dupNick == false){
+            alert("닉네임 중복확인을 해주세요");
+            return;
+        }else{
+            $form.submit();
+        }
+    });
   //중복확인 버튼 클릭
   btnCheck.addEventListener('click',e=>{
     const url = `/members/nickname-check?nickname=${nname.value}`
@@ -26,6 +38,7 @@
                 $nnameCk.innerHTML = '사용가능한 닉네임입니다';
                 $nnameCk.classList.remove('nnameCkRed');
                 $nnameCk.classList.add('nnameCkBlue');
+                dupNick = true;
             } else if (data.header.code == '01') {
                 document.getElementById('nname').style.border = '3px solid red';
                 $nnameCk.innerHTML = '동일한 닉네임이 존재합니다';
