23e86a43835777b5732dcd0997d09f31b0decd95
bakzh <park3096118@gmail.com>
2022-10-06 08:40:07 +0900

    마이페이지 리뷰 수정

--- src/main/resources/static/resource/js/my-page/my_page_activity_review.js
+++ src/main/resources/static/resource/js/my-page/my_page_activity_review.js
@@ -1,3 +1,12 @@
+function askMpDeleteReview(e){
+    if(confirm("리뷰를 삭제하시겠습니까?")){
+        mpDeleteReview(e);
+    }else{
+        return;
+    }
+}
+
+
 //리뷰 삭제
 function mpDeleteReview(e) {
 console.log( e.target.closest('.button-box').id)
