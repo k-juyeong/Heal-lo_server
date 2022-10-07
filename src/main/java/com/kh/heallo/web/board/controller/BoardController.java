d70866368888ef6b61a1828d4f0272bf4e94d4ea
ttkkddms <tkddms603@gmail.com>
2022-10-07 16:51:33 +0900

    게시판

--- src/main/java/com/kh/heallo/web/board/controller/BoardController.java
+++ src/main/java/com/kh/heallo/web/board/controller/BoardController.java
@@ -228,14 +228,20 @@ public class BoardController {
       "/list/{reqPage}//",
       "/list/{reqPage}/{searchType}/{keyword}"})
   public String findAll(
+      HttpServletRequest httpServletRequest,
       @PathVariable(required = false) Optional<Integer> reqPage,
       @PathVariable(required = false) Optional<String> searchType,
       @PathVariable(required = false) Optional<String> keyword,
       @RequestParam(required = false) Optional<String> category,
       Model model) {
+    HttpSession session = httpServletRequest.getSession(false);
+
+    if(session != null)
+    log.info("LoginMember {}",(LoginMember) session.getAttribute(Session.LOGIN_MEMBER.name()));
 
     log.info("/목록list 요청됨{},{},{},{}",reqPage,searchType,keyword,category);
     String cate = getCategory(category);
+
 
     //FindCriteria 값 설정
     fc.getRc().setReqPage(reqPage.orElse(1)); //요청페이지, 요청없으면 1
